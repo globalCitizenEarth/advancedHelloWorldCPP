3c13dccc924a61fd013717c552e2848038a5e5be
WILLIAM BOSWALL <wboswall@gmail.com>
2026-04-24 02:52:08 -0300

    Commit Change
    another test line addition.

--- advancedHelloWorldCPP.cpp
+++ advancedHelloWorldCPP.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+	std::cout << "This is an advanced Hello World program in C++.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
