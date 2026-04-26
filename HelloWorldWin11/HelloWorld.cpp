7015ad5c90f1ceb84c8e8c5f520b39afa4d8cafc
WILLIAM BOSWALL <wboswall@gmail.com>
2026-04-26 00:54:26 -0300

    Commit Files
    commit test source files.  Finally after all the tamper tantrums Visual Studio "Community" 2026 had for at least 4 months it finally is producing solutions but albeit very bloated.  Even for a simple test hello world solution it's at from 100Megs to at least 250Megs.  Very stupid.  While CodeBlocks just has around 14Megs while Linux with some aggressive coaxing, shaves that to 11KB (10.92 kibibytes).

---
+++ HelloWorldWin11/HelloWorld.cpp
@@ -0,0 +1,24 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	std::cout << "This is going to be a great year for C++ programming! Said No one EVER!!\n";
+	std::cout << "This is going to be really sad if this .exe is going to be more than a MebiByte in size!\n";
+	system("Pause");
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ advancedHelloWorldCPPWin11/advancedHelloWorldCPP.cpp
@@ -0,0 +1,27 @@
+// advancedHelloWorldCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	std::cout << "Welcome to C++ programming, The idiotic short bus Microsoft way!!.\n";
+	std::cout << "This is a simple C++ program that prints 'Hello World!' and a welcome message to the console.\n";
+	std::cout << "You can modify this program to include more complex logic, user input, or other features as you learn more about C++ programming.\n";
+	std::cout << "Where Visual Studio fails to even build/compile/test let alone produce a tiny kibibyte executable without this AI bullshit and telementry/metadata shit.\n";
+	std::cout << "This is an advanced Hello World program in C++ but well it can't go further due to the short bus toxic demonic idiocracy bullshit of the fake IT industry and well this fake planet.\n";
+	system("Pause");
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
